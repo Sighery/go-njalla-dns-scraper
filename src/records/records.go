e2d56e7986f0ea184c14a144b950229a2191a6de
Sighery <Sighery@users.noreply.github.com>
2020-02-16 22:54:44 +0100

    Add record type structs with custom unmarshal
    This is done so we get the benefits of type-checking on each Record
    type. Each Record can, and has, slightly different arguments, so it's
    good to have different structs for each differing type. But this means
    we needed a custom unmarshal technique to be able to differentiate at
    run-time what type of Record each was, and assign it to the proper
    ones.
    
    With this commits I've also added tests for example usage.

--- src/records/records.go
+++ src/records/records.go
@@ -1,12 +1,108 @@
 package records
 
-// Record type
-type Record struct {
-	Info map[string]interface{}
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Records []record
+
+func (r *Records) UnmarshalJSON(data []byte) error {
+	// This just splits up the JSON array into the raw JSON for each object
+	var raw []json.RawMessage
+	err := json.Unmarshal(data, &raw)
+	if err != nil {
+		return err
+	}
+
+	for _, x := range raw {
+		// Unmarshal into a map to check the "type" field
+		var obj map[string]interface{}
+		err := json.Unmarshal(x, &obj)
+		if err != nil {
+			return err
+		}
+
+		recordType := ""
+		if t, ok := obj["type"].(string); ok {
+			recordType = t
+		} else {
+			return fmt.Errorf("Record doesn't have field type: %v", obj)
+		}
+
+		// Unmarshal again into the correct type
+		var actual record
+		switch recordType {
+		case "A":
+			actual = &RecordA{}
+		case "AAAA":
+			actual = &RecordAAAA{}
+		case "CNAME":
+			actual = &RecordCNAME{}
+		case "MX":
+			actual = &RecordMX{}
+		case "TXT":
+			actual = &RecordTXT{}
+		case "SRV":
+			actual = &RecordSRV{}
+		case "CAA":
+			actual = &RecordCAA{}
+		case "PTR":
+			actual = &RecordPTR{}
+		case "NS":
+			actual = &RecordNS{}
+		case "TLSA":
+			actual = &RecordTLSA{}
+		case "Redirect":
+			actual = &RecordRedirect{}
+		case "Dynamic":
+			actual = &RecordDynamic{}
+		case "SSHFP":
+			actual = &RecordSSHFP{}
+		default:
+			return fmt.Errorf("Unknown record type: %s", recordType)
+		}
+
+		err = json.Unmarshal(x, actual)
+		if err != nil {
+			return err
+		}
+		*r = append(*r, actual)
+	}
+
+	return nil
 }
 
-// RecordA type
+func (r Records) String() string {
+	representation := ""
+	for _, record := range r {
+		representation += fmt.Sprintf("%+v", record)
+	}
+	return representation
+}
+
+type record interface{}
+
+// A record type
 type RecordA struct {
+	ID      int    `json:"id"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+	TTL     int    `json:"ttl"`
+}
+
+// AAAA record type
+type RecordAAAA struct {
+	ID      int    `json:"id"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+	TTL     int    `json:"ttl"`
+}
+
+// CNAME record type
+type RecordCNAME struct {
 	ID      int    `json:"id"`
 	Type    string `json:"type"`
 	Name    string `json:"name"`
@@ -24,24 +120,87 @@ type RecordMX struct {
 	Priority int    `json:"prio"`
 }
 
-// Records interface implemented by every record
-type Records interface {
-	getValue()
-	getInfo()
-	setType()
+// TXT record type
+type RecordTXT struct {
+	ID      int    `json:"id"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+	TTL     int    `json:"ttl"`
 }
 
-// New constructor
-func New(infoScraped map[string]interface{}) *Record {
-	return &Record{
-		Info: infoScraped,
-	}
+// RecordSRV type
+type RecordSRV struct {
+	ID       int    `json:"id"`
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+	Content  string `json:"content"`
+	TTL      int    `json:"ttl"`
+	Priority int    `json:"prio"`
+	Weight   int    `json:"weight"`
+	Port     int    `json:"port"`
 }
 
-func (r *Record) getValue(key string) interface{} {
-	return r.Info[key]
+// CAA record type
+type RecordCAA struct {
+	ID      int    `json:"id"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+	TTL     int    `json:"ttl"`
 }
 
-func (r *Record) getInfo() map[string]interface{} {
-	return r.Info
+// PTR record type
+type RecordPTR struct {
+	ID      int    `json:"id"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+	TTL     int    `json:"ttl"`
+}
+
+// NS record type
+type RecordNS struct {
+	ID      int    `json:"id"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+	TTL     int    `json:"ttl"`
+}
+
+// TLSA record type
+type RecordTLSA struct {
+	ID      int    `json:"id"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+	TTL     int    `json:"ttl"`
+}
+
+// Redirect record type
+type RecordRedirect struct {
+	ID       int    `json:"id"`
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+	URL      string `json:"content"`
+	Priority int    `json:"prio"`
+}
+
+// Dynamic record type
+type RecordDynamic struct {
+	ID   int    `json:"id"`
+	Type string `json:"type"`
+	Name string `json:"name"`
+	TTL  int    `json:"ttl"`
+}
+
+// SSHFP record type
+type RecordSSHFP struct {
+	ID           int    `json:"id"`
+	Type         string `json:"type"`
+	Name         string `json:"name"`
+	TTL          int    `json:"ttl"`
+	SSHAlgorithm int    `json:"ssh_algorithm"`
+	SSHType      int    `json:"ssh_type"`
+	Content      string `json:"content"`
 }
--- src/records/records_test.go
+++ src/records/records_test.go
@@ -1,34 +1,224 @@
 package records
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
 
-func TestConstructor(t *testing.T) {
-	record := New(map[string]interface{}{
-		"test": 1,
-		"key2": "Privileged",
-	})
-	if record == nil {
-		t.Fail()
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestValidJSONWorks(t *testing.T) {
+	js := []byte(`[
+	{
+		"type": "A",
+		"name": "@",
+		"id": 1,
+		"content": "1.1.1.1",
+		"ttl": 10800
+	},
+	{
+		"type": "AAAA",
+		"name": "@",
+		"id": 2,
+		"content": "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+		"ttl": 10800
+	},
+	{
+		"type": "CNAME",
+		"name": "@",
+		"id": 3,
+		"content": "Content",
+		"ttl": 10800
+	},
+	{
+		"type": "MX",
+		"name": "@",
+		"id": 4,
+		"content": "mail.protonmail.ch",
+		"ttl": 10800,
+		"prio": 10
+	},
+	{
+		"type": "TXT",
+		"name": "@",
+		"id": 5,
+		"content": "v=spf1 a mx ?all",
+		"ttl": 10800
+	},
+	{
+		"type": "SRV",
+		"name": "@",
+		"id": 6,
+		"content": "Content",
+		"ttl": 10800,
+		"prio": 10,
+		"weight": 0,
+		"port": 1234
+	},
+	{
+		"type": "CAA",
+		"name": "@",
+		"id": 7,
+		"content": "0 issue \"letsencrypt.org\"",
+		"ttl": 10800
+	},
+	{
+		"type": "PTR",
+		"name": "@",
+		"id": 8,
+		"content": "Content",
+		"ttl": 10800
+	},
+	{
+		"type": "NS",
+		"name": "Custom",
+		"id": 9,
+		"content": "3-get.njalla.fo",
+		"ttl": 10800
+	},
+	{
+		"type": "TLSA",
+		"name": "@",
+		"id": 10,
+		"content": "3 1 1 BASE64==",
+		"ttl": 10800
+	},
+	{
+		"type": "Redirect",
+		"name": "@",
+		"id": 11,
+		"content": "https://example.com",
+		"prio": 301
+	},
+	{
+		"type": "Dynamic",
+		"name": "@",
+		"id": 12,
+		"ttl": 60
+	},
+	{
+		"type": "SSHFP",
+		"name": "@",
+		"id": 13,
+		"content": "Content",
+		"ttl": 10800,
+		"ssh_algorithm": 4,
+		"ssh_type": 2
+	}
+]`)
+
+	var r Records
+	err := json.Unmarshal(js, &r)
+	if err != nil {
+		t.Fatalf("%q", err)
+	}
+
+	expected := Records{
+		&RecordA{ID: 1, Type: "A", Name: "@", Content: "1.1.1.1", TTL: 10800},
+		&RecordAAAA{
+			ID: 2, Type: "AAAA", Name: "@",
+			Content: "2001:0db8:85a3:0000:0000:8a2e:0370:7334", TTL: 10800,
+		},
+		&RecordCNAME{
+			ID: 3, Type: "CNAME", Name: "@", Content: "Content", TTL: 10800,
+		},
+		&RecordMX{
+			ID: 4, Type: "MX", Name: "@", Content: "mail.protonmail.ch",
+			TTL: 10800, Priority: 10,
+		},
+		&RecordTXT{
+			ID: 5, Type: "TXT", Name: "@", Content: "v=spf1 a mx ?all",
+			TTL: 10800,
+		},
+		&RecordSRV{
+			ID: 6, Type: "SRV", Name: "@", Content: "Content", TTL: 10800,
+			Priority: 10, Weight: 0, Port: 1234,
+		},
+		&RecordCAA{
+			ID: 7, Type: "CAA", Name: "@", TTL: 10800,
+			Content: `0 issue "letsencrypt.org"`,
+		},
+		&RecordPTR{
+			ID: 8, Type: "PTR", Name: "@", Content: "Content", TTL: 10800,
+		},
+		&RecordNS{
+			ID: 9, Type: "NS", Name: "Custom", Content: "3-get.njalla.fo",
+			TTL: 10800,
+		},
+		&RecordTLSA{
+			ID: 10, Type: "TLSA", Name: "@", Content: "3 1 1 BASE64==",
+			TTL: 10800,
+		},
+		&RecordRedirect{
+			ID: 11, Type: "Redirect", Name: "@",
+			URL: "https://example.com", Priority: 301,
+		},
+		&RecordDynamic{ID: 12, Type: "Dynamic", Name: "@", TTL: 60},
+		&RecordSSHFP{
+			ID: 13, Type: "SSHFP", Name: "@", Content: "Content", TTL: 10800,
+			SSHAlgorithm: 4, SSHType: 2,
+		},
+	}
+
+	if rLen, exLen := len(r), len(expected); rLen != exLen {
+		t.Errorf(
+			`Length of parsed [%d] and length of expected [%d] don't match
+Parsed:
+%+v
+
+Expected:
+%+v`,
+			rLen, exLen, r, expected,
+		)
+	}
+
+	for i, e := range expected {
+		testname := fmt.Sprintf("Index: %d", i)
+
+		t.Run(testname, func(t *testing.T) {
+			if rRec := r[i]; !cmp.Equal(e, rRec) {
+				t.Fatalf(
+					"Record from parsed:\n%+v\ndoesn't match with expected:\n%+v",
+					e, rRec,
+				)
+			}
+		})
 	}
 }
 
-func TestGetValue(t *testing.T) {
-	record := New(map[string]interface{}{
-		"test": 1,
-		"key2": "Privileged",
-	})
-	if record.getValue("test") != 1 {
-		t.Fail()
+func TestInvalidTypeFails(t *testing.T) {
+	js := []byte(`[
+	{
+		"type": "MADEUP",
+		"id": 0,
+		"name": "fake",
+		"content": "no"
+	}
+]`)
+
+	var r Records
+	err := json.Unmarshal(js, &r)
+
+	if err == nil || !strings.Contains(err.Error(), "Unknown record type") {
+		t.Fatalf("Test didn't fail as expected")
 	}
 }
 
-func TestGetInfo(t *testing.T) {
-	record := New(map[string]interface{}{
-		"test": 1,
-		"key2": "Privileged",
-	})
-	info := record.getInfo()
-	if info["key2"] == nil {
-		t.Fail()
+func TestMissingTypeFails(t *testing.T) {
+	js := []byte(`[
+	{
+		"id": 0,
+		"name": "fake",
+		"content": "no"
+	}
+]`)
+
+	var r Records
+	err := json.Unmarshal(js, &r)
+
+	if err == nil || !strings.Contains(err.Error(), "doesn't have field type") {
+		t.Fatalf("Test didn't fail as expected")
 	}
 }
